2bc1e108be49628a7ee9cd4208e3563b9e28a77d
djDing <dingdong1366@163.com>
2018-12-30 23:16:34 +0800

    base version

--- circuitBreaker.go
+++ circuitBreaker.go
@@ -1,6 +1,9 @@
 package goBreaker
 
-import ()
+import (
+	"sync"
+	"time"
+)
 
 // State changes between CLOSED, OPEN, HALFOPEN
 // [CLOSED] -->- tripped ----> [OPEN]<-------+
@@ -31,4 +34,255 @@ func (s State) String() string {
 		return "CLOSED"
 	}
 	return "INVALID"
+}
+
+const (
+	// COOLING_TIMEOUT is the time the breaker stay in OPEN before becoming HALFOPEN
+	DEFAULT_COOLING_TIMEOUT = time.Second * 5
+
+	// DETECT_TIMEOUT is the time interval between every detect in HALFOPEN
+	DEFAULT_DETECT_TIMEOUT = time.Millisecond * 200
+
+	// HALFOPEN_SUCCESSES is the threshold when the breaker is in HALFOPEN;
+	// after secceeding consecutively this times, it will change its state from HALFOPEN to CLOSED;
+	DEFAULT_HALFOPEN_SUCCESSES = 2
+)
+
+// TripFunc is a function called by a Breaker when error appears and
+// determines whether the breaker should trip
+type TripFunc func(Container) bool
+
+// StateChangeHandler
+type StateChangeHandler func(oldState, newState State, m Container)
+
+type Breaker struct {
+	Container // contains all success, error and timeout
+	sync.RWMutex
+
+	state           State
+	openTime        time.Time // the time when the breaker become OPEN
+	lastRetryTime   time.Time // last retry time when in HALFOPEN state
+	halfopenSuccess int       // consecutive successes when HALFOPEN
+
+	options Options
+
+	now func() time.Time
+}
+
+// Options for Breaker
+type Options struct {
+	// parameters for container
+	BucketTime time.Duration // the time each bucket holds
+	BucketNums int           // the number of buckets the breaker have
+
+	// parameters for breaker
+	BreakerRate        float64
+	BreakerMinQPS      int           // if qps is over this value, the breaker trip will work
+	BreakerMinSamples  int           // for RateTrip callback
+	CoolingTimeout     time.Duration // fixed when create
+	DetectTimeout      time.Duration // fixed when create
+	HalfOpenSuccess    int
+	ShouldTrip         TripFunc // trip callback, default is RateTrip
+	StateChangeHandler StateChangeHandler
+
+	now func() time.Time
+}
+
+// NewBreaker creates a base breaker with a specified options
+func NewBreaker(options Options) (*Breaker, error) {
+	if options.now == nil {
+		options.now = time.Now
+	}
+
+	if options.BucketTime <= 0 {
+		options.BucketTime = DEFAULT_BUCKET_TIME
+	}
+
+	if options.BucketNums <= 0 {
+		options.BucketNums = DEFAULT_BUCKET_NUMS
+	}
+
+	if options.CoolingTimeout <= 0 {
+		options.CoolingTimeout = DEFAULT_COOLING_TIMEOUT
+	}
+
+	if options.DetectTimeout <= 0 {
+		options.DetectTimeout = DEFAULT_DETECT_TIMEOUT
+	}
+
+	if options.HalfOpenSuccess <= 0 {
+		options.HalfOpenSuccess = DEFAULT_HALFOPEN_SUCCESSES
+	}
+
+	if options.BreakerRate <= 0 {
+		options.BreakerRate = DEFAULT_BREAKER_RATE
+	}
+
+	if options.BreakerMinSamples <= 0 {
+		options.BreakerMinSamples = DEFAULT_BREAKER_MINSAMPLES
+	}
+
+	if options.ShouldTrip == nil {
+		options.ShouldTrip = RateTripFunc(options.BreakerRate, int64(options.BreakerMinSamples))
+	}
+
+	container, err := NewWindowWithOptions(options.BucketTime, options.BucketNums)
+	if err != nil {
+		return nil, err
+	}
+
+	breaker := &Breaker{
+		Container: container,
+		now:       options.now,
+		state:     CLOSED,
+	}
+
+	breaker.options = Options{
+		BucketTime:         options.BucketTime,
+		BucketNums:         options.BucketNums,
+		BreakerRate:        options.BreakerRate,
+		BreakerMinSamples:  options.BreakerMinSamples,
+		CoolingTimeout:     options.CoolingTimeout,
+		DetectTimeout:      options.DetectTimeout,
+		HalfOpenSuccess:    options.HalfOpenSuccess,
+		ShouldTrip:         options.ShouldTrip,
+		StateChangeHandler: options.StateChangeHandler,
+		now:                options.now,
+	}
+
+	return breaker, nil
+}
+
+// Succeed records a success and decreases the concurrency counter by one
+func (b *Breaker) Succeed() {
+	b.Lock()
+	switch b.state {
+	case OPEN: // do nothing
+	case HALFOPEN:
+		b.halfopenSuccess++
+		if b.halfopenSuccess == b.options.HalfOpenSuccess {
+			if b.options.StateChangeHandler != nil {
+				b.options.StateChangeHandler(HALFOPEN, CLOSED, b.Container)
+			}
+			b.Container.Reset()
+			b.state = CLOSED
+		}
+	case CLOSED:
+		b.Container.Succeed()
+	}
+	b.Unlock() // don't use defer
+}
+
+func (b *Breaker) error(isTimeout bool, trip TripFunc) {
+	b.Lock()
+	if isTimeout {
+		b.Container.Timeout()
+	} else {
+		b.Container.Fail()
+	}
+
+	switch b.state {
+	case OPEN: // do nothing
+	case HALFOPEN: // become OPEN
+		if b.options.StateChangeHandler != nil {
+			b.options.StateChangeHandler(HALFOPEN, OPEN, b.Container)
+		}
+		b.openTime = time.Now()
+		b.state = OPEN
+	case CLOSED: // call ShouldTrip
+		if trip != nil && trip(b) {
+			// become OPEN and set the open time
+			if b.options.StateChangeHandler != nil {
+				b.options.StateChangeHandler(CLOSED, OPEN, b.Container)
+			}
+			b.openTime = time.Now()
+			b.state = OPEN
+		}
+	}
+	b.Unlock() // don't use defer
+}
+
+func (b *Breaker) Fail() {
+	b.error(false, b.options.ShouldTrip)
+}
+
+func (b *Breaker) FailWithTrip(trip TripFunc) {
+	b.error(false, trip)
+}
+
+func (b *Breaker) Timeout() {
+	b.error(true, b.options.ShouldTrip)
+}
+
+func (b *Breaker) TimeoutWithTrip(trip TripFunc) {
+	b.error(true, trip)
+}
+
+func (b *Breaker) IsAllowed() bool {
+	return b.isAllowed()
+}
+
+func (b *Breaker) isAllowed() bool {
+	b.Lock()
+	switch b.state {
+	case OPEN:
+		now := time.Now()
+		if b.openTime.Add(b.options.CoolingTimeout).After(now) {
+			b.Unlock()
+			return false
+		}
+		// cooling timeout, then become HALFOPEN
+		b.state = HALFOPEN
+		b.halfopenSuccess = 0
+		b.lastRetryTime = now
+	case HALFOPEN:
+		now := time.Now()
+		if b.lastRetryTime.Add(b.options.DetectTimeout).After(now) {
+			b.Unlock()
+			return false
+		}
+		b.lastRetryTime = now
+	case CLOSED:
+	}
+
+	b.Unlock()
+	return true
+}
+
+// returns the breaker's state now
+func (b *Breaker) State() State {
+	b.Lock()
+	state := b.state
+	b.Unlock()
+	return state
+}
+
+// resets this breaker
+func (b *Breaker) Reset() {
+	b.Lock()
+	b.Container.Reset()
+	b.state = CLOSED
+	b.Unlock()
+}
+
+// ThresholdTripFunc
+func ThresholdTripFunc(threshold int64) TripFunc {
+	return func(m Container) bool {
+		return m.Failures()+m.Timeouts() >= threshold
+	}
+}
+
+// ConsecutiveTripFunc
+func ConsecutiveTripFunc(threshold int64) TripFunc {
+	return func(m Container) bool {
+		return m.ConsecutiveErrors() >= threshold
+	}
+}
+
+// RateTripFunc
+func RateTripFunc(rate float64, minSamples int64) TripFunc {
+	return func(m Container) bool {
+		samples := m.Samples()
+		return samples >= minSamples && m.ErrorRate() >= rate
+	}
 }
---
+++ container.go
@@ -0,0 +1,274 @@
+package goBreaker
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+// Container contains errors, timeouts and successes
+type Container interface {
+	Fail()    // records a failure
+	Succeed() // records a success
+	Timeout() // records a timeout
+
+	Failures() int64          // return the number of failures
+	Successes() int64         // return the number of successes
+	Timeouts() int64          // return the number of timeouts
+	ConsecutiveErrors() int64 // return the consecutive errors recently
+	ErrorRate() float64       // rate = (timeouts + failures) / (timeouts + failures + successes)
+	Samples() int64           // (timeouts + failures + successes)
+	Counts() (successes, failures, timeouts int64)
+
+	Reset()
+}
+
+const (
+	// bucket time is the time each bucket holds
+	DEFAULT_BUCKET_TIME = time.Millisecond * 100
+
+	// bucket nums is the number of buckets the container has;
+	// the more buckets you have, the less counters you lose when
+	// the oldest bucket expire;
+	DEFAULT_BUCKET_NUMS = 100
+
+	// default window size is (DEFAULT_BUCKET_TIME * DEFAULT_BUCKET_NUMS)
+	// it is 10 seconds;
+)
+
+// bucket holds counts of failures and successes
+type bucket struct {
+	failure int64
+	success int64
+	timeout int64
+
+	timeStamp int64 // unix nano time when the bucket was created
+}
+
+// Reset resets the counts to 0 and refreshes the time stamp
+func (b *bucket) Reset() {
+	atomic.StoreInt64(&b.failure, 0)
+	atomic.StoreInt64(&b.success, 0)
+	atomic.StoreInt64(&b.timeout, 0)
+	atomic.StoreInt64(&b.timeStamp, time.Now().UnixNano())
+}
+
+func (b *bucket) Fail() {
+	atomic.AddInt64(&b.failure, 1)
+}
+
+func (b *bucket) Succeed() {
+	atomic.AddInt64(&b.success, 1)
+}
+
+func (b *bucket) Timeout() {
+	atomic.AddInt64(&b.timeout, 1)
+}
+
+func (b *bucket) Failures() int64 {
+	return atomic.LoadInt64(&b.failure)
+}
+
+func (b *bucket) Successes() int64 {
+	return atomic.LoadInt64(&b.success)
+}
+
+func (b *bucket) Timeouts() int64 {
+	return atomic.LoadInt64(&b.timeout)
+}
+
+func (b *bucket) TimeStamp() int64 {
+	return atomic.LoadInt64(&b.timeStamp)
+}
+
+// window maintains a ring of buckets and increments the failure and success
+// counts of the current bucket.
+type window struct {
+	sync.Mutex
+	oldest  int       // oldest bucket index
+	latest  int       // latest bucket index
+	buckets []*bucket // buckets this window holds
+
+	bucketTime time.Duration // time each bucket holds
+	bucketNums int           // the numbe of buckets
+	expireTime time.Duration // expire time of this window
+	inWindow   int           // the number of buckets in the window
+
+	conseErr int64 //consecutive errors
+}
+
+// NewWindow
+func NewWindow() Container {
+	m, _ := NewWindowWithOptions(DEFAULT_BUCKET_TIME, DEFAULT_BUCKET_NUMS)
+	return m
+}
+
+// NewWindowWithOptions creates a new window
+func NewWindowWithOptions(bucketTime time.Duration, bucketNums int) (Container, error) {
+	if bucketNums < 100 {
+		return nil, fmt.Errorf("BucketNums can't be less than 100")
+	}
+
+	w := new(window)
+	w.bucketNums = bucketNums
+	w.bucketTime = bucketTime
+	w.buckets = make([]*bucket, w.bucketNums)
+	w.expireTime = bucketTime * time.Duration(bucketNums)
+	for i := 0; i < w.bucketNums; i++ {
+		w.buckets[i] = new(bucket)
+	}
+
+	w.Reset()
+	return w, nil
+}
+
+// Fail records a failure in the current bucket
+func (w *window) Fail() {
+	w.Lock()
+	b := w.latestBucket()
+	w.conseErr++
+	w.Unlock()
+	b.Fail()
+}
+
+// Success records a success in the current bucket
+func (w *window) Succeed() {
+	w.Lock()
+	b := w.latestBucket()
+	w.conseErr = 0
+	w.Unlock()
+	b.Succeed()
+}
+
+// Timeout records a timeout in the current bucket
+func (w *window) Timeout() {
+	w.Lock()
+	b := w.latestBucket()
+	w.conseErr++
+	w.Unlock()
+	b.Timeout()
+}
+
+func (w *window) Counts() (successes, failures, timeouts int64) {
+	w.Lock()
+	oldest, remain := w.expire()
+	w.Unlock()
+
+	for remain > 0 {
+		timeouts += w.buckets[oldest].Timeouts()
+		successes += w.buckets[oldest].Successes()
+		failures += w.buckets[oldest].Failures()
+		oldest++
+		if oldest >= w.bucketNums {
+			oldest = 0
+		}
+		remain--
+	}
+
+	return
+}
+
+// Successes returns the total number of successes recorded in all buckets
+func (w *window) Successes() int64 {
+	successes, _, _ := w.Counts()
+	return successes
+}
+
+// Failures returns the total number of failures recorded in all buckets
+func (w *window) Failures() int64 {
+	_, failures, _ := w.Counts()
+	return failures
+}
+
+// Timeouts returns the total number of Timeout recorded in all buckets
+func (w *window) Timeouts() int64 {
+	_, _, timeouts := w.Counts()
+	return timeouts
+}
+
+func (w *window) ConsecutiveErrors() int64 {
+	w.Lock()
+	c := w.conseErr
+	w.Unlock()
+	return c
+}
+
+// ErrorRate returns the error rate calculated over all buckets, expressed as
+// a floating point number (ex: 0.9 for 90%)
+func (w *window) ErrorRate() float64 {
+	successes, failures, timeouts := w.Counts()
+
+	if (successes + failures + timeouts) == 0 {
+		return 0.0
+	}
+
+	return float64(failures+timeouts) / float64(successes+failures+timeouts)
+}
+
+func (w *window) Samples() int64 {
+	successes, failures, timeouts := w.Counts()
+
+	return successes + failures + timeouts
+}
+
+// Reset resets this window
+func (w *window) Reset() {
+	w.Lock()
+	w.oldest = 0
+	w.latest = 0
+	w.inWindow = 1
+	w.conseErr = 0
+	w.buckets[w.latest].Reset()
+	w.Unlock()
+}
+
+// expire removes expired buckets's affect
+// lock should be obtained by the outside function who call it
+func (w *window) expire() (oldest, inWindow int) {
+	now := time.Now().UnixNano()
+	for w.inWindow > 0 {
+		oldestBucket := w.buckets[w.oldest]
+		stamp := oldestBucket.TimeStamp()
+
+		if stamp+int64(w.expireTime) <= now { // expired
+			w.oldest++
+			if w.oldest == w.bucketNums {
+				w.oldest = 0
+			}
+			w.inWindow--
+		} else {
+			break
+		}
+	}
+	return w.oldest, w.inWindow
+}
+
+// latestBucket returns the latest bucket;
+// lock should be obtained by the outside function who call it
+func (w *window) latestBucket() *bucket {
+	// check or create the lastest bucket
+	lastestBucket := w.buckets[w.latest]
+	stamp := lastestBucket.TimeStamp()
+	now := time.Now().UnixNano()
+
+	if stamp+int64(w.bucketTime) <= now { // expired
+		w.latest++
+		if w.latest >= w.bucketNums {
+			w.latest = 0
+		}
+
+		if w.inWindow == w.bucketNums {
+			// the lastest covered the oldest(latest == oldest)
+			w.oldest++
+			if w.oldest >= w.bucketNums {
+				w.oldest = 0
+			}
+		} else {
+			w.inWindow++
+		}
+		w.buckets[w.latest].Reset()
+	}
+	b := w.buckets[w.latest]
+	return b
+}
